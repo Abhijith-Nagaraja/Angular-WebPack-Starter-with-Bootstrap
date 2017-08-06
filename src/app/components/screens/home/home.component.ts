93af4e0004d3be5cf9125b9001f0c3e1ae6c5773
Abhijith Nagaraja <abhijith.n@leantaas.com>
2017-08-06 01:38:07 -0700

    Bootstrap issue

--- src/app/components/screens/home/home.component.ts
+++ src/app/components/screens/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+declare var jQuery: any;
 @Component({
     selector: 'home',  // <home></home>
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+    loadModal() {
+        jQuery("#confirm").modal("show");
+    }
 }
